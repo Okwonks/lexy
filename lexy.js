efa74f8f37e44831b23696f860101178ce414f40
Okwonks <albertoketch@gmail.com>
2017-10-21 21:17:41 +0300

    Adding the slack controller to the porject and setting up the environment for the slack bot.

--- lexy.js
+++ lexy.js
@@ -1,4 +1,28 @@
-var username = require('.env').username;
-var password = require('.env').password;
-var workspaceId = require('.env').workspace_id;
+/** Imports that are required from the .env file */
+var watsonUser = require('.env').WATSON_USER_NAME;
+var watsonPassword = require('.env').WATSON_PASSWORD;
+var watsonWorkspaceId = require('.env').WATSON_WORKSPACE_ID;
+var slackToken = require('.env').SLACK_BOT_TOKEN;
+
+var slackController = Botkit.slackBot();
+
+// Spawning the slack bot.
+var slackBot = slackController.spawn({
+    token: slackToken
+})
+
+// Connection to the Watson Conversation service
+var watsonMiddleware = require('botkit-middleware-watson')({
+    username: watsonUser,
+    password: watsonPassword,
+    workspace_id: watsonWorkspaceId,
+    version_date: '2017-05-26',
+    minimum_confidence: 0.50
+})
+
+// Telling the bot what to do with the incoming messages
+slackController.middleware.receive.use(watsonMiddleware.receive);
+slackBot.startRTM();
+
+// Making the bot reply to messages using watson.
 
