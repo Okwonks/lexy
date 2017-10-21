99380c062afd9524f2074f21befce6c4c23a113c
Okwonks <albertoketch@gmail.com>
2017-10-22 01:04:48 +0300

    Refactoring the code to making the botkit work.

---
+++ slack_bot.js
@@ -0,0 +1,35 @@
+var Botkit = require('botkit'); // Botkit module required in makiing the bot
+/** Imports that are required from the .env file */
+var watsonUser = require('.env').WATSON_USER_NAME;
+var watsonPassword = require('.env').WATSON_PASSWORD;
+var watsonWorkspaceId = require('.env').WATSON_WORKSPACE_ID;
+var slackToken = require('.env').SLACK_BOT_TOKEN;
+
+var slackController = Botkit.slackbot(); // slackbot set ups the controller.
+
+// Spawning the slack bot.
+var slackBot = slackController.spawn({
+    token: slackToken
+})
+
+// Connection to the Watson Conversation service
+var watsonMiddleware = require('botkit-middleware-watson')({
+    username: watsonUser,
+    password: watsonPassword,
+    workspace_id: watsonWorkspaceId,
+    version_date: '2017-05-26',
+    minimum_confidence: 0.50
+})
+
+// Telling the bot what to do with the incoming messages
+slackController.middleware.receive.use(watsonMiddleware.receive);
+slackBot.startRTM();
+
+// Making the bot reply to messages using watson.
+slackController.hears(['.*'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    if (message.watsonError) {
+        bot.reply(message, "I'm sorry but for technical reasons I cannot respond to your message");
+    } else {
+        bot.reply(message, message.watsonData.output.text.join('\n'));
+    }
+})
