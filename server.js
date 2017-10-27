c60cba26ab936bdd74434dce66280800411cefb5
Okwonks <albertoketch@gmail.com>
2017-10-27 22:50:53 +0300

    Altering server.js

--- server.js
+++ server.js
@@ -14,3 +14,26 @@ require('./app')(app);
 app.listen(port, function() {
     console.log('Client server listening on port' + port);
 });
+app.get('/', (req, res) => {
+    
+    // Parse params from the webhook verification request
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Check if a token and mode were sent
+    if (mode && token) {
+    
+      // Check the mode and token sent are correct
+      if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        
+        // Respond with 200 OK and challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
